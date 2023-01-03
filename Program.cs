3d192532ec76baf77b2466763c60721ba57b1e5b
kubrabm <kubrabm@code.edu.az>
2023-01-04 02:00:16 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,240 @@
+﻿namespace Task_2023_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n= int.Parse(Console.ReadLine());
+            //int[]A= new int[n];
+            //int count = 0;
+            //int sum = 0;
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    A[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    if (A[i] < 0)
+            //    {
+            //        sum += A[i];
+            //        count++;
+            //    }
+            //}
+
+
+            //Console.WriteLine($"menfi ededlerin cemi {sum}");
+            //Console.WriteLine($"menfi ededlerin sayi {count}");
+            #endregion Task 1
+
+            #region Task 2
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] A = new int[n];
+            //int count = 0;
+            //int sum = 0;
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    A[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    if (A[i] % 5 == 0 || A[i] % 7 != 0)
+            //    {
+            //        sum += A[i];
+            //        count++;
+            //    }
+
+            //}
+            //Console.WriteLine($"5-e bolunen ve 7-ye bolunmeyen ededlerin cemi {sum}");
+            //Console.WriteLine($"5-e bolunen ve 7-ye bolunmeyen ededlerin sayi {count}");
+
+            #endregion Task 2
+
+            #region Task 3
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] A = new int[n];
+            //int count = 0;
+            //int sum = 0;
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    A[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    if (A[i] % 2 != 0 & A[i] > 0)
+            //    {
+            //        sum += A[i];
+            //        count++;
+            //    }
+
+            //}
+            //Console.WriteLine($"musbet ve tek ededlerin cemi {sum} ");
+            //Console.WriteLine($"musbet ve tek ededlerin sayi {count} ");
+            #endregion Task3
+
+            #region Task 4
+
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] A = new int[n];
+            //int count = 0;
+            //int sum = 0;
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    A[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < A.Length; i += 2)
+            //{
+            //    if (A[i] % 2 == 0)
+
+            //    {
+            //        sum += A[i];
+            //        count++;
+            //    }
+
+            //}
+            //Console.WriteLine($"cut yerde duran cut ededlerin cemi {sum} ");
+            //Console.WriteLine($"cut yerde duran cut ededlerin sayi {count} ");
+
+            #endregion Task 4
+
+            #region Task 5
+
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] A = new int[n];
+            //int count = 0;
+            //int sum = 0;
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    A[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    if (A[i] % 2 == 0 && i%2!=0)
+
+            //    {
+            //        sum += A[i];
+            //        count++;
+            //    }
+
+            //}
+            //Console.WriteLine($"tek yerde duran cut ededlerin cemi {sum} ");
+            //Console.WriteLine($"tek yerde duran cut ededlerin sayi {count} ");
+            #endregion Task 5
+
+            #region Task 6
+
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] A = new int[n];
+            //int count = 0;
+            //int sum = 0;
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    A[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < A.Length; i += 2)
+            //{
+            //    if (A[i] % 2 != 0)
+
+            //    {
+            //        sum += A[i];
+            //        count++;
+            //    }
+
+
+
+            //}
+            //Console.WriteLine($"cut yerde duran tek ededlerin cemi {sum} ");
+            //Console.WriteLine($"cut yerde duran tek ededlerin sayi {count} ");
+
+            #endregion Task 6
+
+            #region Task 7
+
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] A = new int[n];
+            //int count = 0;
+
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    A[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int i = 1; i < A.Length; i++)
+            //{
+            //    if (A[i] % 7 == 1 || A[i] % 7 == 2 || A[i] % 7 == 5)
+
+            //    {
+            //        count++;
+            //    }
+
+            //}
+            //Console.WriteLine($"7-ye bolende qaliqi 1,2 ve 5 olan ededlerin sayi {count} ");
+            #endregion Task 7
+
+            #region Task 9
+
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] A = new int[n];
+            //int max = 0;
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    A[i] = int.Parse(Console.ReadLine());
+            //}
+            //foreach (var item in A)
+            //{
+            //    if (item > max)
+            //    {
+            //        max = item;
+            //    }
+            //}
+
+            //Console.WriteLine($"A-daki maksimal eded {max} ");
+            #endregion Task 9
+
+            #region Task 10
+
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] A = new int[n];
+            //int min = 0;
+
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    A[i] = int.Parse(Console.ReadLine());
+            //}v
+
+
+            //foreach (var item in A)
+            //{
+            //    if (item < min)
+            //    {
+            //        min = item;
+            //    }
+            //}
+
+            //Console.WriteLine($"A-daki minimal eded {min} ");
+            #endregion Task 10
+        }
+    }
+}
